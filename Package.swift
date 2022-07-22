2f7b12f311c3ef2bb1713681c060b0a739b0b28a
Deepak <deepak@airtime.com>
2022-07-22 17:22:30 -0400

    update package name

--- Package.swift
+++ Package.swift
@@ -2,26 +2,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "link",
-    platforms: [.iOS(.v13)],
+    name: "link-presence",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
-            name: "link",
-            targets: ["linktarget"]
+            name: "link-presence",
+            targets: ["link-presence"]
         ),
     ],
     targets: [
-        .target(
-            name: "linktarget",
-            dependencies: [
-                "link"
-            ],
-            path: "linkwrapper"
-        ),
         .binaryTarget(
-            name: "link",
-            url: "https://github.com/deepoke/Link-Presence/releases/download/0.1.0/link-0.1.0.zip",
-            checksum: "3beaec877eca6fdee53dd0254046887e299fa2008a486092cc84346a4d9a4bda"
-        )
+            name: "link-presence",
+            url: "https://github.com/deepoke/Link-Presence/releases/download/0.1.0/link-presence-0.1.0.zip",
+            checksum: "64f3bf2e468570aa1cec1f1f3019b0658da335bd4fb8fe89896d558f8782666e"
+        ),
     ]
 )
